c3f329e9092bc96df7a4dc6e36622c7d683b29d4
ElSuca <springtrap370@gmail.com>
2024-02-19 12:10:59 -0300

    tuki

--- www/js/codigo.js
+++ www/js/codigo.js
@@ -11,208 +11,292 @@ const loading = document.createElement("ion-loading");
 Inicio();
 
 function MostrarLoader(texto) {
-  loading.cssClass = "my-custom-class";
-  loading.message = texto;
-  //loading.duration = 2000;
-  document.body.appendChild(loading);
-  loading.present();
+	loading.cssClass = "my-custom-class";
+	loading.message = texto;
+	//loading.duration = 2000;
+	document.body.appendChild(loading);
+	loading.present();
 }
 
 function Inicio() {
-  Eventos();
-  ArmarMenuOpciones();
+	Eventos();
+	ArmarMenuOpciones();
 }
 function ArmarMenuOpciones() {
-  let HayToken = localStorage.getItem("token");
-  let _menu = `<ion-item href="/" onclick="cerrarMenu()">Home</ion-item>`;
+	let hayKey = localStorage.getItem("apiKey");
+	let _menu = `<ion-item href="/" onclick="cerrarMenu()">Inicio</ion-item>`;
 
-  if (HayToken) {
-    _menu += ` <ion-item href="/regAlimento" onclick="cerrarMenu()">Registrar Alimento</ion-item>
+	if (hayKey) {
+		_menu += ` <ion-item href="/regAlimento" onclick="cerrarMenu()">Registrar Alimento</ion-item>
                 <ion-item href="/listar" onclick="cerrarMenu()">Listar Alimentos</ion-item>
                 <ion-item onclick="Logout()">Log out</ion-item>`;
-  } else {
-    _menu += `<ion-item href="/login" onclick="cerrarMenu()">Log in</ion-item>
+	} else {
+		_menu += `<ion-item href="/login" onclick="cerrarMenu()">Log in</ion-item>
                   <ion-item href="/registro" onclick="cerrarMenu()">Registrarse</ion-item>`;
-  }
+	}
 
-  document.querySelector("#menuOpciones").innerHTML = _menu;
+	document.querySelector("#menuOpciones").innerHTML = _menu;
 }
 
 function Logout() {
-  localStorage.clear();
-  ArmarMenuOpciones();
-  NAV.push("page-home");
-  MENU.close();
+	localStorage.clear();
+	ArmarMenuOpciones();
+	NAV.push("page-home");
+	MENU.close();
 }
 function Eventos() {
-  ROUTER.addEventListener("ionRouteDidChange", Navegar);
-  document
-    .querySelector("#btnRegistrar")
-    .addEventListener("click", TomarDatosRegistro);
-  document
-    .querySelector("#btnLogin")
-    .addEventListener("click", TomarDatosLogin);
+	ROUTER.addEventListener("ionRouteDidChange", Navegar);
+	document
+		.querySelector("#btnRegistrar")
+		.addEventListener("click", TomarDatosRegistro);
+	document
+		.querySelector("#btnLogin")
+		.addEventListener("click", TomarDatosLogin);
 }
 
 function TomarDatosLogin() {
-  let e = document.querySelector("#txtUsuarioLogin").value;
-  let p = document.querySelector("#txtPasswordLogin").value;
+	let nombreUsuario = document.querySelector("#txtUsuarioLogin").value;
+	let password = document.querySelector("#txtPasswordLogin").value;
 
-  IniciarSesion(e, p);
+	IniciarSesion(nombreUsuario, password);
 }
 
-function IniciarSesion(e, p) {
-  let usuario = new Object();
-  usuario.email = e;
-  usuario.password = p;
-  MostrarLoader("Espere...");
-  fetch("https://ort-tallermoviles.herokuapp.com/api/usuarios/session", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(usuario),
-  })
-    .then(function (response) {
-      console.log(response);
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      DetenerLoader();
-      if (data.error == "") {
-        localStorage.setItem("token", data.data.token);
-        ArmarMenuOpciones();
-        NAV.push("page-home");
-      } else {
-        MostrarToast("Error: " + data.error, 5000, "danger");
-        //document.querySelector("#resLog").innerHTML = data.error;
-      }
-    });
+function IniciarSesion(nombreUsuario, password) {
+	let usuario = new Object();
+	usuario.usuario = nombreUsuario;
+	usuario.password = password;
+	MostrarLoader("Espere...");
+	fetch("https://calcount.develotion.com/login.php", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(usuario),
+	})
+		.then(function (response) {
+			console.log(response);
+			return response.json();
+		})
+		.then(function (data) {
+			console.log(data);
+			DetenerLoader();
+			if (data.codigo == 200) {
+				MostrarToast("Exito!", 3000, "success");
+				localStorage.setItem("apiKey", data.apiKey);
+				localStorage.setItem("idUser", data.id);
+				localStorage.setItem("caloriasDiarias", data.caloriasDiarias);
+				ArmarMenuOpciones();
+				NAV.push("page-home");
+			} else {
+				MostrarToast("Error: " + data.error, 5000, "danger");
+				//document.querySelector("#resLog").innerHTML = data.error;
+			}
+		});
 }
 
 function TomarDatosRegistro() {
-  let n = document.querySelector("#txtNombreRegistro").value;
-  let a = document.querySelector("#txtApellidoRegistro").value;
-  let e = document.querySelector("#txtEmailRegistro").value;
-  let d = document.querySelector("#txtDireccionRegistro").value;
-  let p = document.querySelector("#txtPasswordRegistro").value;
+	let nombreUsuario = document.querySelector("#txtUsuarioRegistro").value;
+	let password = document.querySelector("#txtPasswordRegistro").value;
+	let pais = document.querySelector("#slcPais").value;
+	let calorias = document.querySelector("#txtCaloriasRegistro").value;
 
-  Registrar(n, a, e, d, p);
+	Registrar(nombreUsuario, password, pais, calorias);
 }
 
-function Registrar(n, a, e, d, p) {
-  let usuario = new Object();
-  usuario.nombre = n;
-  usuario.apellido = a;
-  usuario.email = e;
-  usuario.direccion = d;
-  usuario.password = p;
+function Registrar(nombreUsuario, password, pais, calorias) {
+	let usuario = new Object();
+	usuario.usuario = nombreUsuario;
+	usuario.password = password;
+	usuario.idPais = pais;
+	usuario.caloriasDiarias = calorias;
+	fetch("https://calcount.develotion.com/usuarios.php", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(usuario),
+	})
+		.then(function (response) {
+			console.log(response);
+			return response.json();
+		})
+		.then(function (data) {
+			console.log(data);
 
-  fetch("https://ort-tallermoviles.herokuapp.com/api/usuarios", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(usuario),
-  })
-    .then(function (response) {
-      console.log(response);
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
+			if (data.codigo == 200) {
+				MostrarToast("Exito!", 3000, "success");
+				localStorage.setItem("apiKey", data.apiKey);
+				ArmarMenuOpciones();
+				NAV.push("page-home");
+			} else {
+				MostrarToast(data.mensaje, 5000, "danger");
+			}
+		});
+}
 
-      if (data.error == "") {
-        document.querySelector("#resReg").innerHTML = "Alta correcta";
-      } else {
-        document.querySelector("#resReg").innerHTML = data.error;
-      }
-    });
+function PoblarSelect() {
+	MostrarLoader("Espere...");
+	document.querySelector("#slcPais").innerHTML = "";
+	document.querySelector("#slcPais").innerHTML =
+		"<div slot='label'>País <ion-text color='danger'>(Requerido)</ion-text></div>";
+
+	fetch("https://calcount.develotion.com/paises.php")
+		.then(function (response) {
+			console.log(response);
+			return response.json();
+		})
+		.then(function (data) {
+			console.log(data);
+			Paises = data.paises;
+			for (let p of Paises) {
+				document.querySelector(
+					"#slcPais"
+				).innerHTML += `<ion-select-option value="${p.id}">${p.name}</ion-select-option>`;
+			}
+			DetenerLoader();
+		});
 }
 
 function Navegar(evt) {
-  const RUTA = evt.detail.to;
-  console.log(evt);
-  OcultarTodo();
-  if (RUTA == "/") {
-    HOME.style.display = "block";
-  } else if (RUTA == "/login") {
-    LOGIN.style.display = "block";
-  } else if (RUTA == "/registro") {
-    REGISTRO.style.display = "block";
-  } else if (RUTA == "/regAlimento") {
-    REGALIMENTO.style.display = "block";
-  } else if (RUTA == "/listar") {
-    LISTAR.style.display = "block";
-  }
+	const RUTA = evt.detail.to;
+	console.log(evt);
+	OcultarTodo();
+	if (RUTA == "/") HOME.style.display = "block";
+	else if (RUTA == "/login") LOGIN.style.display = "block";
+	else if (RUTA == "/registro") {
+		REGISTRO.style.display = "block";
+		PoblarSelect();
+	} else if (RUTA == "/regAlimento") {
+		REGALIMENTO.style.display = "block";
+		PoblarSelectAlimento();
+	} else if (RUTA == "/listar") {
+		LISTAR.style.display = "block";
+		ObtenerAlimentos();
+	}
 }
 function cerrarMenu() {
-  MENU.close();
+	MENU.close();
 }
 
 function OcultarTodo() {
-  HOME.style.display = "none";
-  LOGIN.style.display = "none";
-  REGISTRO.style.display = "none";
-  REGALIMENTO.style.display = "none";
-  LISTAR.style.display = "none";
+	HOME.style.display = "none";
+	LOGIN.style.display = "none";
+	REGISTRO.style.display = "none";
+	REGALIMENTO.style.display = "none";
+	LISTAR.style.display = "none";
 }
 
 function DetenerLoader() {
-  loading.dismiss();
+	loading.dismiss();
 }
 
 function Alertar(titulo, subtitulo, mensaje) {
-  const alert = document.createElement("ion-alert");
-  alert.cssClass = "my-custom-class";
-  alert.header = titulo;
-  alert.subHeader = subtitulo;
-  alert.message = mensaje;
-  alert.buttons = ["OK"];
-  document.body.appendChild(alert);
-  alert.present();
+	const alert = document.createElement("ion-alert");
+	alert.cssClass = "my-custom-class";
+	alert.header = titulo;
+	alert.subHeader = subtitulo;
+	alert.message = mensaje;
+	alert.buttons = ["OK"];
+	document.body.appendChild(alert);
+	alert.present();
 }
 
 function MostrarToast(mensaje, duracion, color) {
-  const toast = document.createElement("ion-toast");
-  toast.message = mensaje;
-  toast.duration = duracion;
-  if (color == "") toast.color = undefined;
-  else toast.color = color;
-  document.body.appendChild(toast);
-  toast.present();
+	const toast = document.createElement("ion-toast");
+	toast.message = mensaje;
+	toast.duration = duracion;
+	if (color == "") toast.color = undefined;
+	else toast.color = color;
+	document.body.appendChild(toast);
+	toast.present();
 }
 
-// function ObtenerProductos() {
-//   fetch("https://ort-tallermoviles.herokuapp.com/api/productos", {
-//     method: "GET",
-//     headers: {
-//       "Content-Type": "application/json",
-//       "x-auth": localStorage.getItem("token"),
-//     },
-//   })
-//     .then(function (response) {
-//       console.log(response);
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//       let cadena =
-//         "<ion-card> <ion-card-header> <ion-card-title>Card Title</ion-card-title>" +
-//         "<ion-card-subtitle>Card Subtitle</ion-card-subtitle> </ion-card-header> <ion-card-content> <ion-list>";
-//       for (let p of data.data) {
-//         cadena += `
-//           <ion-item>
-//           <ion-thumbnail slot="start">
-//           <img alt="Silhouette of mountains" src="https://ort-talleresmoviles.herokuapp.com/assets/imgs/${urlImagen}"/>
-//           </ion-thumbnail>
-//           <ion-label>
-//           p.nombre
-//           </ion-label>
-//           </ion-item>`;
-//       }
-//       cadena += "</ion-list> </ion-card-content> </ion-card>";
-//       document.querySelector("#lista-productos").innerHTML = cadena;
-//     });
-// }
+function ObtenerAlimentos() {
+	MostrarLoader("Espere...");
+	fetch(
+		`https://calcount.develotion.com/registros.php?idUsuario=${localStorage.getItem(
+			"idUser"
+		)}`,
+
+		{
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				apikey: localStorage.getItem("apiKey"),
+				iduser: localStorage.getItem("idUser"),
+			},
+		}
+	)
+		.then(function (response) {
+			console.log(response);
+			return response.json();
+		})
+		.then(function (data) {
+			console.log(data);
+			let cadena = "<ion-card> <ion-list>";
+			let icono = "";
+			if (data.registros.length != 0) {
+				for (let alimento of data.registros) {
+					icono = alimento.imagen + ".png";
+					cadena += `
+				<ion-item>
+				<ion-thumbnail slot="start">
+				  <img alt="Imagen de ${alimento.nombre}" src="https://calcount.develotion.com/imgs/${icono}" />
+				</ion-thumbnail>
+				<ion-label>
+				<h2>${alimento.nombre}</h2>
+				<p>Calorías: ${alimento.calorias} kcal</p>
+				</ion-label>
+				<ion-button fill="outline" color="danger">Eliminar</ion-button>
+			  </ion-item>
+		`;
+				}
+				cadena += "</ion-list> </ion-card-content> </ion-card> <br>";
+				document.querySelector("#lista-alimentos").innerHTML = cadena;
+			} else {
+				let cadena = `
+			<ion-card>
+				<ion-card-header>
+					<ion-card-title>Oops!</ion-card-title>
+					<ion-card-subtitle>Parece que no tenés ningún registro todavía...</ion-card-subtitle>
+				</ion-card-header>
+				<ion-card-content>
+					<ion-button size="small" fill="outline" color="success" href="/regAlimento">Crear uno</ion-button>
+				</ion-card-content>
+			</ion-card>`;
+				document.querySelector("#lista-alimentos").innerHTML = cadena;
+			}
+			DetenerLoader();
+		});
+}
+
+function PoblarSelectAlimento() {
+	MostrarLoader("Espere...");
+	fetch(`https://calcount.develotion.com/alimentos.php`,
+
+		{
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				apikey: localStorage.getItem("apiKey"),
+				iduser: localStorage.getItem("idUser"),
+			},
+		}
+	)
+		.then(function (response) {
+			console.log(response);
+			return response.json();
+		})
+		.then(function (data) {
+			console.log(data);
+			AlimentoLista = data.alimentos;
+			for (let alimento of AlimentoLista) {
+				document.querySelector(
+					"#slcAlimento"
+				).innerHTML += `<ion-select-option value="${alimento.id}">${alimento.nombre}</ion-select-option>`;
+			}
+		});
+	DetenerLoader();
+}
+
+
