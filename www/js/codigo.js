7f5bae1bf9573529ae5124aa9ad19133d9d9007b
ElSuca <springtrap370@gmail.com>
2024-02-22 10:54:53 -0300

    tuki938

--- www/js/codigo.js
+++ www/js/codigo.js
@@ -10,6 +10,11 @@ const loading = document.createElement("ion-loading");
 
 Inicio();
 
+function Inicio() {
+  Eventos();
+  ArmarMenuOpciones();
+}
+
 function MostrarLoader(texto) {
   loading.cssClass = "my-custom-class";
   loading.message = texto;
@@ -18,10 +23,10 @@ function MostrarLoader(texto) {
   loading.present();
 }
 
-function Inicio() {
-  Eventos();
-  ArmarMenuOpciones();
+function DetenerLoader() {
+  loading.dismiss();
 }
+
 function ArmarMenuOpciones() {
   let hayKey = localStorage.getItem("apiKey");
   let _menu = `<ion-item href="/" onclick="cerrarMenu()">Inicio</ion-item>`;
@@ -38,12 +43,6 @@ function ArmarMenuOpciones() {
   document.querySelector("#menuOpciones").innerHTML = _menu;
 }
 
-function Logout() {
-  localStorage.clear();
-  ArmarMenuOpciones();
-  NAV.push("page-home");
-  MENU.close();
-}
 function Eventos() {
   ROUTER.addEventListener("ionRouteDidChange", Navegar);
   document
@@ -55,6 +54,12 @@ function Eventos() {
   document
     .querySelector("#btnRegAlimento")
     .addEventListener("click", TomarDatosAlimento);
+  document
+    .querySelector("#btnFiltrar")
+    .addEventListener("click", FiltrarXFecha);
+  document
+    .querySelector("#btnMapa")
+    .addEventListener("click", MostrarUsuariosEnMapa);
 }
 
 function TomarDatosLogin() {
@@ -95,6 +100,13 @@ function IniciarSesion(nombreUsuario, password) {
         //document.querySelector("#resLog").innerHTML = data.error;
       }
     });
+}
+
+function Logout() {
+  localStorage.clear();
+  ArmarMenuOpciones();
+  NAV.push("page-home");
+  MENU.close();
 }
 
 function TomarDatosRegistro() {
@@ -164,8 +176,16 @@ function Navegar(evt) {
   const RUTA = evt.detail.to;
   console.log(evt);
   OcultarTodo();
-  if (RUTA == "/") HOME.style.display = "block";
-  else if (RUTA == "/login") LOGIN.style.display = "block";
+  if (RUTA == "/") {
+    HOME.style.display = "block";
+    MostrarCalorias();
+    document.querySelector("#cardMapa").style.display = "none";
+    let hayKey = localStorage.getItem("apiKey");
+    if (hayKey) {
+      document.querySelector("#cardMapa").style.display = "block";
+      CrearMapa();
+    }
+  } else if (RUTA == "/login") LOGIN.style.display = "block";
   else if (RUTA == "/registro") {
     REGISTRO.style.display = "block";
     PoblarSelect();
@@ -175,10 +195,12 @@ function Navegar(evt) {
     fechaAlimento.max = new Date().toISOString(); //solo dios sabe como esto funciona
   } else if (RUTA == "/listar") {
     LISTAR.style.display = "block";
-    ObtenerRegistros();
+    document.querySelector("#btnFiltrar").style.display = "none";
+    ListarRegistros();
     filtroRegistros.max = new Date().toISOString();
   }
 }
+
 function cerrarMenu() {
   MENU.close();
 }
@@ -189,10 +211,6 @@ function OcultarTodo() {
   REGISTRO.style.display = "none";
   REGALIMENTO.style.display = "none";
   LISTAR.style.display = "none";
-}
-
-function DetenerLoader() {
-  loading.dismiss();
 }
 
 function Alertar(titulo, subtitulo, mensaje) {
@@ -214,93 +232,6 @@ function MostrarToast(mensaje, duracion, color) {
   else toast.color = color;
   document.body.appendChild(toast);
   toast.present();
-}
-
-function EliminarRegistro(id) {
-  id = id.substring(11);
-  MostrarLoader("Espere...");
-  fetch(`https://calcount.develotion.com/registros.php?idRegistro=${id}`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-      apikey: localStorage.getItem("apiKey"),
-      iduser: localStorage.getItem("idUser"),
-    },
-  })
-    .then(function (response) {
-      console.log(response);
-      ObtenerRegistros();
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      if (data.codigo == 200) MostrarToast("Exito!", 3000, "success");
-      else MostrarToast(data.mensaje, 3000, "danger");
-    });
-  DetenerLoader();
-}
-async function ObtenerRegistros() {
-  MostrarLoader("Espere...");
-  fetch(
-    `https://calcount.develotion.com/registros.php?idUsuario=${localStorage.getItem(
-      "idUser"
-    )}`,
-
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        apikey: localStorage.getItem("apiKey"),
-        iduser: localStorage.getItem("idUser"),
-      },
-    }
-  )
-    .then(function (response) {
-      console.log(response);
-      return response.json();
-    })
-    .then(async function (data) {
-      console.log(data);
-      let cadena = "<ion-card> <ion-list>";
-      let icono = "";
-      if (data.registros.length != 0) {
-        for (let registro of data.registros) {
-          let listaAlimentos = await ObtenerAlimentos();
-          let alimento = undefined;
-          for (let a of listaAlimentos.alimentos) {
-            if (registro.idAlimento == a.id) alimento = a;
-          }
-          icono = alimento.imagen + ".png";
-          cadena += `
-				<ion-item>
-				<ion-thumbnail slot="start">
-				  <img alt="Imagen de ${alimento.nombre}" src="https://calcount.develotion.com/imgs/${icono}" />
-				</ion-thumbnail>
-				<ion-label>
-				<h2>${alimento.nombre}</h2>
-				<p>Calorías: ${alimento.calorias} kcal</p>
-				</ion-label>
-				<ion-button fill="outline" color="danger" id="btnEliminar${registro.id}" onclick="EliminarRegistro(this.id)">Eliminar</ion-button>
-			  </ion-item>
-		`;
-        }
-        cadena += "</ion-list> </ion-card-content> </ion-card> <br>";
-        document.querySelector("#lista-alimentos").innerHTML = cadena;
-      } else {
-        let cadena = `
-			<ion-card>
-				<ion-card-header>
-					<ion-card-title>Oops!</ion-card-title>
-					<ion-card-subtitle>Parece que no tenés ningún registro todavía...</ion-card-subtitle>
-				</ion-card-header>
-				<ion-card-content>
-					<ion-button size="small" fill="outline" color="success" href="/regAlimento">Crear uno</ion-button>
-				</ion-card-content>
-			</ion-card>`;
-        document.querySelector("#lista-alimentos").innerHTML = cadena;
-      }
-      DetenerLoader();
-    });
 }
 
 async function ObtenerAlimentos() {
@@ -341,25 +272,25 @@ async function TomarDatosAlimento() {
   }
   let encontrado = false;
   fecha = fecha.split("T")[0];
-  let valorXPorcion = 0;
-  let total = undefined;
+  // let valorXPorcion = 0;
+  // let total = undefined;
   MostrarLoader("Espere...");
   let lista = await ObtenerAlimentos();
   DetenerLoader();
   encontrado = false;
-  for (let a of lista.alimentos) {
-    if (a.id == alimento) {
-      for (let i = 0; i <= a.porcion.length || !encontrado; i++) {
-        let caracter = a.porcion.charAt(i);
-        if (caracter == "g" || caracter == "m" || caracter == "u") {
-          valorXPorcion = parseInt(a.porcion.substring(0, i));
-          encontrado = true;
-        }
-      }
-    }
-  }
-  total = valorXPorcion * cantidad;
-  RegistrarAlimento(alimento, total, fecha);
+  // for (let a of lista.alimentos) {
+  //   if (a.id == alimento) {
+  //     for (let i = 0; i <= a.porcion.length || !encontrado; i++) {
+  //       let caracter = a.porcion.charAt(i);
+  //       if (caracter == "g" || caracter == "m" || caracter == "u") {
+  //         valorXPorcion = parseInt(a.porcion.substring(0, i));
+  //         encontrado = true;
+  //       }
+  //     }
+  //   }
+  // }
+  // total = valorXPorcion * cantidad;
+  RegistrarAlimento(alimento, cantidad, fecha);
 }
 
 function RegistrarAlimento(alimento, total, fecha) {
@@ -391,6 +322,224 @@ function RegistrarAlimento(alimento, total, fecha) {
     });
 }
 
+async function ObtenerRegistros() {
+  let promesaRegistro = await fetch(
+    `https://calcount.develotion.com/registros.php?idUsuario=${localStorage.getItem(
+      "idUser"
+    )}`,
+
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        apikey: localStorage.getItem("apiKey"),
+        iduser: localStorage.getItem("idUser"),
+      },
+    }
+  );
+  console.log(promesaRegistro);
+  let listaRegistros = await promesaRegistro.json();
+  return listaRegistros;
+}
+
+async function ListarRegistros() {
+  document.querySelector("#btnFiltrar").style.display = "none";
+  MostrarLoader("Espere...");
+  let listaRegistros = await ObtenerRegistros();
+  let cadena = "<ion-card> <ion-list>";
+  let icono = "";
+  if (listaRegistros.registros.length != 0) {
+    for (let registro of listaRegistros.registros) {
+      let listaAlimentos = await ObtenerAlimentos();
+      let alimento = undefined;
+      for (let a of listaAlimentos.alimentos) {
+        if (registro.idAlimento == a.id) alimento = a;
+      }
+      icono = alimento.imagen + ".png";
+      cadena += `
+				<ion-item>
+				<ion-thumbnail slot="start">
+				  <img alt="Imagen de ${
+            alimento.nombre
+          }" src="https://calcount.develotion.com/imgs/${icono}" />
+				</ion-thumbnail>
+				<ion-label>
+				<h2>${alimento.nombre}</h2>
+				<p>Calorías: ${alimento.calorias * registro.cantidad} kcal</p>
+        <p>Cantidad: ${registro.cantidad}</p>
+				</ion-label>
+				<ion-button fill="outline" color="danger" id="btnEliminar${
+          registro.id
+        }" onclick="EliminarRegistro(this.id)">Eliminar</ion-button>
+			  </ion-item>
+		`;
+    }
+    cadena += "</ion-list> </ion-card-content> </ion-card> <br>";
+    document.querySelector("#lista-alimentos").innerHTML = cadena;
+  } else {
+    let cadena = `
+			<ion-card>
+				<ion-card-header>
+					<ion-card-title>Oops!</ion-card-title>
+					<ion-card-subtitle>Parece que no tenés ningún registro todavía...</ion-card-subtitle>
+				</ion-card-header>
+				<ion-card-content>
+					<ion-button size="small" fill="outline" color="success" href="/regAlimento">Crear uno</ion-button>
+				</ion-card-content>
+			</ion-card>`;
+    document.querySelector("#lista-alimentos").innerHTML = cadena;
+  }
+  DetenerLoader();
+}
+
+function EliminarRegistro(id) {
+  id = id.substring(11);
+  MostrarLoader("Espere...");
+  fetch(`https://calcount.develotion.com/registros.php?idRegistro=${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      apikey: localStorage.getItem("apiKey"),
+      iduser: localStorage.getItem("idUser"),
+    },
+  })
+    .then(function (response) {
+      console.log(response);
+      ListarRegistros();
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      if (data.codigo == 200) MostrarToast("Exito!", 3000, "success");
+      else MostrarToast(data.mensaje, 3000, "danger");
+    });
+  DetenerLoader();
+}
+
+async function MostrarCalorias() {
+  let hayKey = localStorage.getItem("apiKey");
+  if (hayKey) {
+    MostrarLoader("Espere...");
+    let listaRegistros = await ObtenerRegistros();
+    let listaAlimentos = await ObtenerAlimentos();
+    let caloriasTotales = 0;
+    let caloriasDiarias = 0;
+    let fechaActual = new Date().toISOString();
+    let limite = localStorage.getItem("caloriasDiarias");
+    fechaActual = fechaActual.split("T")[0];
+    for (let registro of listaRegistros.registros) {
+      for (let alimento of listaAlimentos.alimentos) {
+        if (registro.idAlimento == alimento.id) {
+          caloriasTotales += alimento.calorias * registro.cantidad;
+        }
+      }
+    }
+    let cadena = `<ion-card-header>
+    <ion-card-title>Calorias Totales</ion-card-title>
+    <ion-card-subtitle>${caloriasTotales}</ion-card-subtitle>
+    </ion-card-header>`;
+    document.querySelector("#cardCalTotales").innerHTML = cadena;
+    for (let registro of listaRegistros.registros) {
+      for (let alimento of listaAlimentos.alimentos) {
+        if (
+          registro.idAlimento == alimento.id &&
+          registro.fecha == fechaActual
+        ) {
+          caloriasDiarias += alimento.calorias * registro.cantidad;
+        }
+      }
+    }
+    let modificadorLimite = (limite * 10) / 100;
+    cadena = `<ion-card-header>
+    <ion-card-title>Calorias Diarias</ion-card-title>`;
+    if (caloriasDiarias > limite)
+      cadena += `<ion-card-subtitle color="danger">${caloriasDiarias}</ion-card-subtitle>`;
+    else if (
+      caloriasDiarias < limite &&
+      caloriasDiarias >= limite - modificadorLimite
+    )
+      cadena += `<ion-card-subtitle color="warning">${caloriasDiarias}</ion-card-subtitle>`;
+    else
+      cadena += `<ion-card-subtitle color="success">${caloriasDiarias}</ion-card-subtitle>`;
+    cadena += `</ion-card-header>`;
+    document.querySelector("#cardCalDiarias").innerHTML = cadena;
+    DetenerLoader();
+  } else {
+    let cadena = `<ion-card-header><ion-card-title>Bienvenido!</ion-card-title>
+    <ion-card-subtitle>Por favor, inicia sesión para acceder a las funcionalidades.</ion-card-subtitle></ion-card-header>`;
+    document.querySelector("#cardCalTotales").innerHTML = cadena;
+  }
+}
+
+function MostrarBtnFiltro() {
+  document.querySelector("#btnFiltrar").style.display = "block";
+}
+
+async function FiltrarXFecha() {
+  let fechas = document.querySelector("#filtroRegistros").value;
+  console.log(fechas);
+  let fecha1;
+  let fecha2;
+  if (fechas.length == 2) {
+    MostrarLoader("Espere...");
+    let listaRegistros = await ObtenerRegistros();
+    let cadena = "<ion-card> <ion-list>";
+    let icono = "";
+    if (listaRegistros.registros.length != 0) {
+      for (let registro of listaRegistros.registros) {
+        if (fechas[0] > fechas[1]) {
+          fecha1 = fechas[1];
+          fecha2 = fechas[0];
+        } else {
+          fecha1 = fechas[0];
+          fecha2 = fechas[1];
+        }
+        if (registro.fecha >= fecha1 && registro.fecha <= fecha2) {
+          let listaAlimentos = await ObtenerAlimentos();
+          let alimento = undefined;
+          for (let a of listaAlimentos.alimentos) {
+            if (registro.idAlimento == a.id) alimento = a;
+          }
+          icono = alimento.imagen + ".png";
+          cadena += `
+          <ion-item>
+          <ion-thumbnail slot="start">
+            <img alt="Imagen de ${
+              alimento.nombre
+            }" src="https://calcount.develotion.com/imgs/${icono}" />
+          </ion-thumbnail>
+          <ion-label>
+          <h2>${alimento.nombre}</h2>
+          <p>Calorías: ${alimento.calorias * registro.cantidad} kcal</p>
+          </ion-label>
+          <ion-button fill="outline" color="danger" id="btnEliminar${
+            registro.id
+          }" onclick="EliminarRegistro(this.id)">Eliminar</ion-button>
+          </ion-item>
+      `;
+        }
+      }
+      cadena += "</ion-list> </ion-card-content> </ion-card> <br>";
+      document.querySelector("#lista-alimentos").innerHTML = cadena;
+    } else {
+      let cadena = `
+        <ion-card>
+          <ion-card-header>
+            <ion-card-title>Oops!</ion-card-title>
+            <ion-card-subtitle>Parece que no tenés ningún registro todavía...</ion-card-subtitle>
+          </ion-card-header>
+          <ion-card-content>
+            <ion-button size="small" fill="outline" color="success" href="/regAlimento">Crear uno</ion-button>
+          </ion-card-content>
+        </ion-card>`;
+      document.querySelector("#lista-alimentos").innerHTML = cadena;
+    }
+    DetenerLoader();
+  } else {
+    MostrarToast("Por favor, ingrese solo dos(2) fechas.", 5000, "danger");
+  }
+}
+
 var map;
 
 function CrearMapa() {
@@ -408,4 +557,10 @@ function SetearCoordenadas(pos) {
   console.log(pos);
   latitud = pos.coords.latitude;
   longitud = pos.coords.longitude;
+}
+
+async function MostrarUsuariosEnMapa() {
+  let cantidadUsuarios = Number(
+    document.querySelector("#txtCantidadUsuarios").value
+  );
 }
